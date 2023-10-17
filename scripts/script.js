92a542e00465303ee3bc3171b4ebce3f56054139
Wayne Chen <wchen206@my.bcit.ca>
2023-10-17 12:54:43 -0700

    firebase

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,14 @@
 //---------------------------------
 
 
-function sayHello() {
-    //do something
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
 }
-//sayHello();    //invoke function
